357df1d0c4ea47e6f1c008cda7a4230bf5e42455
Erilcem Yilmaz <eril.yilmaz@hotmail.com>
2024-11-12 10:46:45 +0100

    refactor

--- test.py
+++ test.py
@@ -1,59 +1,53 @@
-from SIA import (
-    export_artikel5_name,
-    export_artikel5_name_leagealSeatid,
-    export_artikel5_uid,
+from SIA.artikel5 import (
+    export_by_name,
+    export_by_name_and_city,
+    export_by_name_and_legal_seat_id,
+    export_by_uid,
 )
 from SIA_Nextcloud_Automation import creat_new_user, search_for_user
 
 
-def test_compare_and_update():
-    export_artikel5_name.compare_and_update(
+def run_export_by_name():
+    export_by_name.compare_and_update(
         r"C:\coding\test_Data\artikel5_mail.csv",
-        r"C:\coding\test_Data\results\artikel5_mail_results.csv",
+        r"C:\coding\test_Data\results\20241112_artikel5_mail_results.csv",
     )
 
 
-# test_compare_and_update()
-
-
-def test_compare_and_update2():
-    export_artikel5_name_leagealSeatid.compare_and_update(
+def run_export_by_name_and_legal_seat_id():
+    export_by_name_and_legal_seat_id.compare_and_update(
         r"C:\coding\test_Data\artikel5_test_2.csv",
-        r"C:\coding\test_Data\results\artikel5_test_results.csv",
+        r"C:\coding\test_Data\results\20241112_artikel5_test_results.csv",
     )
 
 
-# test_compare_and_update2()
-
-
-def test_compare_and_update_uid():
-    export_artikel5_uid.compare_and_update(
+def run_export_by_uid():
+    export_by_uid.compare_and_update(
         r"C:\coding\test_Data\artikel5_mail_uid.csv",
-        r"C:\coding\test_Data\results\artikel5_mail_uid_results.csv",
+        r"C:\coding\test_Data\results\20241112_artikel5_mail_uid_results.csv",
     )
 
 
-def test_export_shab():
-    export_artikel5_uid.compare_and_update(
-        r"C:\coding\test_Data\artikel5_mail_uid.csv",
-        r"C:\coding\test_Data\results\artikel5_mail_uid_results.csv",
+def run_export_by_name_and_city():
+    export_by_name_and_city.compare_and_update(
+        r"C:\coding\test_Data\20240924_any_corporate_wo_UID.csv",
+        r"C:\coding\test_Data\results\20240924_any_corporate_wo_UID_result_new.csv",
     )
 
 
-# test_compare_and_update_uid()
-
-
-def test_create_user_Cloud():
-
+def run_create_user_cloud():
     creat_new_user.main(r"C:\coding\test_Data\nextcloud_test.CSV")
 
 
-# test_create_user_Cloud()
-
-
-def test_active_user_Cloud():
-
+def run_search_user_cloud():
     search_for_user.main(r"C:\coding\test_Data\nextcloud_test.CSV")
 
 
-# test_active_user_Cloud()
+if __name__ == "__main__":
+    # Uncomment the function you want to run
+    run_export_by_name()
+    # run_export_by_name_and_legal_seat_id()
+    # run_export_by_uid()
+    # run_export_by_name_and_city()
+    # run_create_user_cloud()
+    # run_search_user_cloud()
